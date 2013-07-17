c761d16bb53e805bbc663b09101ddd078240746d
BindChat <bindchatinc@gmail.com>
2013-07-18 00:42:11 +0800

    未登陆用户也可以看到聊天部件

--- bindchat.class.php
+++ bindchat.class.php
@@ -27,31 +27,7 @@ class plugin_bindchat_livechat {
         $return = '';
 
         if($bindchat_livechat_siteid) {
-
-            //include template('bindchat_livechat:widget');
-$return = <<<EOF
-<!--Start of BindChat Live Chat Script -->
-<script type="text/javascript">
-  (function (w, d, e, g, r, a, m) {
-      w['BindChatObject'] = r;
-      w[r] = w[r] || function () {
-          (w[r].q = w[r].q || []).push(arguments)
-      }, w[r].t = 1 * new Date();
-      a = d.createElement(e),
-      m = d.getElementsByTagName(e)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', '//www.bindchat.com/api/js/all.js', 'bindchat');
-
-  bindchat('create', '{$bindchat_livechat_siteid}', '{$bindchat_livechat_sitedomain}');
-</script>
-<noscript>
-<a href="http://www.bindchat.com/sites/contact/{$bindchat_livechat_siteid}" target="_blank">Feedback?</a> 
-powered by <a href="http://www.bindchat.com/welcome">Bindchat</a>
-</noscript>
-<!--End of BindChat Live Chat Script -->
-EOF;
+            include template('bindchat_livechat:widget');
         }
         return $return;
     }
